c6d411e92339c25dcce8ad1ee57678e0a2979b1b
Alp Hancioglu <alp.hancioglu@ad.aie.edu>
2016-11-03 21:38:49 +1100

    DragonHp

--- LOTA/New Unity Project/Assets/Scripts/DragonStuff/DragonAbilities.cs
+++ LOTA/New Unity Project/Assets/Scripts/DragonStuff/DragonAbilities.cs
@@ -5,11 +5,13 @@ using UnityEngine.Networking;
 public class DragonAbilities : NetworkBehaviour
 {
     private float timeStamp;
-    public float coolDownPeriodofInSeconds;
+    private float coolDownPeriodofInSeconds;
 
     private Animator anim;
 
     public float distanceQ;
+
+    public float Damage;
 
     public GameObject BigFB;
     public GameObject FireBolt;
@@ -17,6 +19,8 @@ public class DragonAbilities : NetworkBehaviour
     public GameObject FlameThrower;
     public GameObject MeteorUltimate;
 
+    public float bulletSpeed = 10f;
+   
     private Vector3 point;   
 
     public float m_LaunchForce = 30f;
@@ -56,10 +60,13 @@ public class DragonAbilities : NetworkBehaviour
 
         if (Input.GetButtonDown("Fire1") && timeStamp <= 0)
         {
+
             timeStamp = coolDownPeriodofInSeconds;
 
-            GameObject X = Instantiate(FireBolt, transform.position + transform.forward * distanceQ + transform.up * 4.5f, transform.rotation) as GameObject;
-            X.transform.parent = transform;
+           Quaternion targrot = transform.rotation;
+
+            GameObject X = Instantiate(FireBolt, transform.position + transform.forward * distanceQ + transform.up * 4.5f, targrot) as GameObject;
+            X.transform.position = X.transform.position + transform.forward * bulletSpeed;
         }
 
         if (Input.GetButtonDown("Fire2") && timeStamp <= 0)
@@ -93,7 +100,11 @@ public class DragonAbilities : NetworkBehaviour
         if (Input.GetButtonDown("Space"))
         {
             anim.SetTrigger("IsJumping");
-            Debug.Log("jump");
         }
-    }   
+    }
+
+   void CalculateDamage()
+    {
+
+    } 
 }
