e95c7f56735e400aa7508ed09a8a6dc723710a1f
Alp Hancioglu <alp.hancioglu@ad.aie.edu>
2016-11-22 20:41:07 +1100

    AXE

---
+++ LOTA/New Unity Project/Assets/Scripts/AXE/AxeAbilities.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class AxeAbilities : NetworkBehaviour
+{
+    private float timeStamp;
+    private float coolDownPeriodofInSeconds;
+    private Animator anim;
+    public float smoothTime = 50f;
+    public GameObject Brute;
+
+    [HideInInspector]
+    public GameObject EAbilityTarget;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 targetpos;
+    private bool QAbillityUseBool = false;
+    private bool mouseReady = false;
+    private float QAblittyUseTimer = 3f;
+    private float QAblittyCD;
+    private float EAbilityCD = 0;
+    private bool EButtonDown = false;
+
+    private Vector3 point;
+    private float distance;
+
+    public GameObject sword;
+
+    [SyncVar]
+    public Quaternion dir;
+
+    PlayerUIController uiControl;
+
+
+
+    void Awake()
+    {
+        uiControl = GetComponent<PlayerUIController>();
+    }
+
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        timeStamp = coolDownPeriodofInSeconds;
+    }
+
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane mouseplane = new Plane(transform.up, transform.position);
+
+        timeStamp -= Time.deltaTime;
+
+
+        if (mouseplane.Raycast(ray, out distance))
+        {
+            point = ray.GetPoint(distance);
+
+        }
+
+
+        if (Input.GetKeyDown("return"))
+        {
+            timeStamp = 0;
+        }
+
+        dir = transform.rotation;
+        if (isLocalPlayer)
+        {
+
+
+            if (Input.GetButtonDown("Fire1") && timeStamp <= 0)
+            {
+                anim.SetTrigger("LeftClick");
+                sword.GetComponent<SwordDamage>().Damage = 500;
+            }
+
+            if (Input.GetButtonDown("Fire2"))
+            {
+                anim.SetTrigger("RightClick");
+                sword.GetComponent<SwordDamage>().Damage = 750;
+            }
+
+          //  QAbility();
+
+            //EAbility();
+
+        }
+
+    }
+}
---
+++ LOTA/New Unity Project/Assets/Scripts/AXE/AxeMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class AxeMovement : NetworkBehaviour {
+
+    private Animator anim;
+    public float speed;
+    private GameObject cam;
+    public float stunTime;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        cam = Camera.main.gameObject;
+
+        if (!isLocalPlayer)
+        {
+            Destroy(this);
+            return;
+        }
+    }
+
+    
+    void Update ()
+    {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+
+        if (H != 0 || V != 0)
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+
+
+        if (gameObject.GetComponent<Health>().Stuned <= 0)
+        {
+            transform.position = transform.position + Vector3.right * H * speed * Time.deltaTime;
+            transform.position = transform.position + Vector3.forward * V * speed * Time.deltaTime;
+        }
+
+        cam.transform.position = transform.position + new Vector3(0, 70, -5);
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane mouseplane = new Plane(transform.up, transform.position);
+
+        float distance;
+        if (mouseplane.Raycast(ray, out distance))
+        {
+            Vector3 point = ray.GetPoint(distance);
+            transform.LookAt(point);
+        }
+	}
+}
